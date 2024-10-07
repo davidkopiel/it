80b09f38c0d0631d7cd0bd9dad510e73e1369ea1
Eytan <73502334+BiggieFudge@users.noreply.github.com>
2024-10-07 16:37:45 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='frogbotTest',                          # Replace with your project name
+    version='0.1.0',                           # Version of your project
+    description='A brief description of your project',  # Short description
+    long_description=open('README.md').read(), # Long description from README file
+    long_description_content_type='text/markdown',  # Content type of long description
+    author='Your Name',                        # Your name
+    author_email='your.email@example.com',     # Your email address
+    url='https://github.com/BiggieFudge/frogbotTest',  # Your project's repository URL
+    packages=find_packages(),                   # Automatically find packages
+    python_requires='>=3.6',                    # Python version requirement
+    install_requires=[                          # List your project dependencies
+        'Flask',
+        'requests',
+        'jinja2','jfrog-curation-malicious-dummy',
+    ],
+)
